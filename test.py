197ae9ba15a9778e0a841830e86e1c1453fa8d9f
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-11-12 21:13:30 +0100

    Initial commit

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 import chainlit as cl
-from openai import OpenAI
+import os
 
-client = OpenAI()
+os.environ['OPENAI_API_KEY'] = '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'
+
+
+# return everything that the user inputs
+@cl.on_message
+async def main(message: str):
+    message = input()
+    await cl.Message(content=message).send()
+
