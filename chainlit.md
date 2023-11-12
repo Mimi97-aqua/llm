c6d4542dbe666571cf7502c1913898303da98ddb
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-11-12 15:02:48 +0100

    Modifed code according to new standards

--- chainlit.md
+++ chainlit.md
@@ -1,1 +1,1 @@
-### Welcome to my CocoBot
+### Welcome to CocoBot
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import chainlit as cl
-import openai
+from openai import OpenAI
 import os
 
-os.environ['OPENAI_API_KEY'] = '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'
+# os.environ['OPENAI_API_KEY'] = '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'
+
+client = OpenAI(
+    api_key= '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'
+)
+
 
 # return everything that the user input
-
-
 @cl.on_message
-async def main(message : str):
-    await cl.Message(content = message).send()
+async def main(message: str):
+    # pass message into chatgpt api
+    response = client.chat.completions.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            {"role": "assistant",
+             "content": "You are a helpful assistant"},
+            {"role": "user",
+             "content": message},
+        ],
+        temperature=1,
+    )
+    await cl.Message(content=response).send()
