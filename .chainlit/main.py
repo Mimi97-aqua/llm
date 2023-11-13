f03ee6dc6cf4536faf47f128bbbf7ab2663adc2b
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-11-13 08:22:42 +0100

    Successfully returned everything that the user enters

---
+++ .chainlit/main.py
@@ -0,0 +1,12 @@
+import chainlit as cl
+import os
+
+os.environ['OPENAI_API_KEY'] = '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'
+
+
+# return everything that the user inputs
+@cl.on_message
+async def main(message: str):
+    message = input('Go enter your prompt')
+    await cl.Message(content=message).send()
+
--- test.py
+++ test.py
@@ -1,12 +1,8 @@
 import chainlit as cl
-import os
-
-os.environ['OPENAI_API_KEY'] = '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'
 
 
-# return everything that the user inputs
 @cl.on_message
-async def main(message: str):
-    message = input()
-    await cl.Message(content=message).send()
-
+async def main(message: cl.Message):
+    # return everything that the user enters
+    await cl.Message(
+        content=f' {message.content}', ).send()
