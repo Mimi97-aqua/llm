d5186482ccd7c3fe386e47dfac51893d8be597c4
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-11-13 10:46:00 +0100

    Tested Langchain LLM friendliness using the PromptTemplate module

--- langchain_integration.py
+++ langchain_integration.py
@@ -2,9 +2,13 @@ import chainlit as cl
 import os
 import openai
 from openai import OpenAI
-from langchain import PromptTemplate, OpenAI, LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.llms.openai import OpenAI
 
 template = """ 
     Question: {question}
     Answer: Let's think step by step
     """
+
+print(template.format(question='What is 1+1'))
