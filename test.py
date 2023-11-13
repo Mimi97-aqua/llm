5cd5105f7451e0a033d684fe2d5c327643fd63a4
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-11-13 09:05:20 +0100

    Fixed issue with API key

--- test.py
+++ test.py
@@ -1,15 +1,26 @@
 import chainlit as cl
 import os
-import openai
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
-api_key = os.getenv('OPENAPI_KEY')
-openai.api_key = api_key
+api_key = os.getenv('OPENAI_API_KEY')
+print(api_key)
+client = OpenAI(api_key=api_key)  # Instantiating OpenAI client with API Key
 
 
 @cl.on_message
 async def main(message: cl.Message):
+    # pass the message into the chatgpt api
+    response = client.chat.completions.create(
+        model='gpt-3.5-turbo',
+        temperature=1,
+        messages=[
+            {'role': 'assistant', 'content': 'You are a helpful assistant'},
+            {'role': 'user', 'content': message}
+        ]
+    )
+
     # return everything that the user enters
     await cl.Message(
         content=f' {message.content}', ).send()
