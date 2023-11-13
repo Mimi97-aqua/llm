e22601092fbabb1841ee1dc5f1335e8dc34a5bc1
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-11-13 15:00:58 +0100

    Fix: Unrelated responses from model

--- langchain_integration.py
+++ langchain_integration.py
@@ -1,6 +1,5 @@
 import chainlit as cl
 import os
-import openai
 from openai import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
@@ -9,21 +8,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
-client = OpenAI(api_key=api_key)
 
 template = """ 
         Question: {question}
-        Answer: Let's think step by step
+        Answer: {answer}
         """
 
 
 @cl.on_chat_start
 def main():
     # Variables to initiate as soon as chainlit UI is deployed
-    prompt = PromptTemplate(template=template, input_variables=['question'])
+    prompt = PromptTemplate(template=template, input_variables=['question', 'answer'])
     llm_chain = LLMChain(
         prompt=prompt,
-        llm=OpenAI(temperature=1, streaming=True),
+        llm=OpenAI(api_key=api_key, temperature=1, streaming=True),
         verbose=True
     )
 
@@ -34,6 +32,15 @@ def main():
 async def main(message: cl.Message):
     llm_chain = cl.user_session.get('llm_chain')
 
+    # Initial answer
+    answer = ''
+
+    # Processing user's input
     result = await llm_chain.acall(message, callbacks=[cl.AsyncLangchainCallbackHandler()])
 
-    await cl.Message(content=result['text']).send()
+    # Get generated answer from result
+    if 'text' in result:
+        answer = result['text']
+
+    # Send respond back to user
+    await cl.Message(content=answer).send()
