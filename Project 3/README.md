82cd3a7c221648b16730311b744921a13d5b6523
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-11-14 11:52:35 +0100

    First test with arxiv

--- Project 3/README.md
+++
@@ -1,7 +0,0 @@
-# LLM
-Large Language Models [Tutorial](https://youtu.be/xZDB1naRUlk?si=IxDqmmd35XrimPDJ)
-
-### Project 3
-GPT Researcher
-
-## Stages
--- Project 3/internet_browsing_Arxiv_Naive.py
+++ Project 3/internet_browsing_Arxiv_Naive.py
@@ -8,3 +8,20 @@ load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
 openai = OpenAI(api_key=api_key)
 
+llm = ChatOpenAI(temperature=0.3)
+tools = load_tools(
+    ['arxiv']
+)
+
+agent_chain = initialize_agent(
+    tools,
+    llm,
+    max_iterations=5,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,  # Algorithm for LLM
+    verbose=True,
+    handle_parsing_errors=True
+)
+
+agent_chain.run(
+    'What is LIFO?'
+)
