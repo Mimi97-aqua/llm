a148836a22eb60ead520f342c98cee8a518735ed
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-11-14 08:41:57 +0100

    document_qa.py

--- Project 2/document_qa.py
+++ Project 2/document_qa.py
@@ -50,3 +50,83 @@ def get_docsearch(file: AskFileResponse):
         docs, embeddings
     )
     return docsearch
+
+@cl.on_chat_start
+async def start():
+    # Sending image with local file path
+    await cl.Message(content='You can now chat with your PDFs').send()
+    files = None
+
+    while files is None:
+        files = await cl.AskFileMessage(
+            content=welcome_message,
+            accept=['text/plain', 'application/pdf'],
+            max_size_mb=20,
+            timeout=180
+        ).send()
+
+    file = files[0]
+
+    msg = cl.Message(content=f'Processing "{file.name}"...')
+    await msg.send()
+
+    # No async implementation in the Pinecone client, fallback to sync
+    docsearch = await cl.make_async(get_docsearch)(file)
+
+    chain = RetrievalQAWithSourcesChain.from_chain_type(
+        ChatOpenAI(temperature=0, streaming=True),
+        chain_type="stuff",
+        retriever=docsearch.as_retriever(max_tokens_limit=4097),
+    )
+
+    # Let the user know that the system is ready
+    msg.content = f"`{file.name}` processed. You can now ask questions!"
+    await msg.update()
+
+    cl.user_session.set("chain", chain)
+
+
+@cl.on_message
+async def main(message):
+    chain = cl.user_session.get("chain")  # type: RetrievalQAWithSourcesChain
+    cb = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+    )
+    cb.answer_reached = True
+    res = await chain.acall(message, callbacks=[cb])
+
+    answer = res["answer"]
+    sources = res["sources"].strip()
+    source_elements = []
+
+    # Get the documents from the user session
+    docs = cl.user_session.get("docs")
+    metadatas = [doc.metadata for doc in docs]
+    all_sources = [m["source"] for m in metadatas]
+
+    if sources:
+        found_sources = []
+
+        # Add the sources to the message
+        for source in sources.split(","):
+            source_name = source.strip().replace(".", "")
+            # Get the index of the source
+            try:
+                index = all_sources.index(source_name)
+            except ValueError:
+                continue
+            text = docs[index].page_content
+            found_sources.append(source_name)
+            # Create the text element referenced in the message
+            source_elements.append(cl.Text(content=text, name=source_name))
+
+        if found_sources:
+            answer += f"\nSources: {', '.join(found_sources)}"
+        else:
+            answer += "\nNo sources found"
+
+    if cb.has_streamed_final_answer:
+        cb.final_stream.elements = source_elements
+        await cb.final_stream.update()
+    else:
+        await cl.Message(content=answer, elements=source_elements).send()
