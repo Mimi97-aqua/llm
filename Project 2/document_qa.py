2f13a91d3d531efef2560ba78543dee5ae13c920
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-11-14 08:08:09 +0100

    Additions: document_qa.py

--- Project 2/document_qa.py
+++ Project 2/document_qa.py
@@ -8,11 +8,30 @@ from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.chat_models import ChatOpenAI
 from chainlit.types import AskFileResponse
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-embeddings = OpenAIEmbeddings()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)  # Chunk size = 1000 characters
+embeddings = OpenAIEmbeddings()  # Uses Ada2 Model
 
 welcome_message = """
 Welcome to the Chainlit PDF QA demo! To get started:
 1. Upload a PDF of text file
 2. Ask a question about the file
 """
+
+
+def process_file(file: AskFileResponse):
+    import tempfile
+
+    if file.type == 'text/plain':
+        Loader = TextLoader
+    elif file.type == 'application/pdf':
+        Loader = PyPDFLoader
+
+    with tempfile.NamedTemporaryFile() as tempfile:
+        tempfile.write(file.content)
+        loader = Loader(tempfile.name)
+        documents = loader.load()
+        docs = text_splitter.split_documents(documents)
+
+        for i, doc in enumerate(docs):
+            doc.metadata('source') = f'source_{i}'
+        return docs
