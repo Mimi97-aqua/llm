b07a78b944d7a1c040314234a7d6905ae4a6983c
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-11-14 16:43:19 +0100

    Code complete; human_as_a_tool.py

--- Project 3/human_as_a_tool.py
+++ Project 3/human_as_a_tool.py
@@ -9,3 +9,19 @@ import openai
 load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
 openai = OpenAI(api_key=api_key)
+
+llm = ChatOpenAI(temperature=0.5)
+math_llm = OpenAI(temperature=0.5)
+tools = load_tools(
+    ['human', 'llm-math'],
+    llm=math_llm
+)
+
+agent_chain = initialize_agent(
+    tools,
+    llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True
+)
+
+agent_chain.run("what is my math problem and its solution")
